d3939304272dd72ef460718186107c6571cca835
Anuyta <ryabtseva.anna.92@mail.ru>
2018-03-07 15:31:59 +0300

    hw_4 done
    copy 5 files with TPL

---
+++ Homework_4/Homework_4/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Homework_4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnFrom = new System.Windows.Forms.Button();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(32, 38);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(60, 17);
+            label1.TabIndex = 0;
+            label1.Text = "От куда";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(35, 102);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(40, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Куда";
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(104, 13);
+            this.tbFrom.Multiline = true;
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(354, 74);
+            this.tbFrom.TabIndex = 2;
+            this.tbFrom.Click += new System.EventHandler(this.tbFrom_TextChanged);
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(104, 102);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(354, 22);
+            this.tbTo.TabIndex = 3;
+            this.tbTo.Click += new System.EventHandler(this.tbFrom_TextChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(35, 141);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(423, 30);
+            this.progressBar.TabIndex = 4;
+            // 
+            // btnFrom
+            // 
+            this.btnFrom.Location = new System.Drawing.Point(480, 28);
+            this.btnFrom.Name = "btnFrom";
+            this.btnFrom.Size = new System.Drawing.Size(109, 30);
+            this.btnFrom.TabIndex = 5;
+            this.btnFrom.Text = "Файлы...";
+            this.btnFrom.UseVisualStyleBackColor = true;
+            this.btnFrom.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(480, 98);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(109, 30);
+            this.btnTo.TabIndex = 6;
+            this.btnTo.Text = "Папка...";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(480, 141);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(109, 30);
+            this.btnCopy.TabIndex = 7;
+            this.btnCopy.Text = "Копировать";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(248, 196);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(109, 35);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 243);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.btnFrom);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.tbFrom);
+            this.Controls.Add(label2);
+            this.Controls.Add(label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnFrom;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Homework_4/Homework_4/MainForm.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_4
+{
+    public partial class MainForm : Form
+    {
+        private List<string> pathesFrom = new List<string>();
+        private string pathTo;
+        private List<Task> tasksLst = new List<Task>();
+        private CancellationTokenSource tokenSource;
+        private CancellationToken token;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            tokenSource = new CancellationTokenSource();
+            token = tokenSource.Token;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn == btnFrom)
+            {
+                tbFrom.Text = GetFilesName();
+            }
+            else if (btn == btnTo)
+            {
+                tbTo.Text = GetFolderName();
+                pathTo = tbTo.Text;
+            }
+            else if (btn == btnCopy)
+            {
+                StartCopy();
+            }
+            else if(btn == btnCancel)
+            {
+                CancellationCopy();
+            }
+        }
+
+        private void tbFrom_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb == tbFrom)
+            {
+                GetFilesNames(tbFrom.Text);
+            }
+            else if (tb == tbTo)
+            {
+                pathTo = tbTo.Text;
+            }
+        }
+
+        private void tbTo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                StartCopy();
+            }
+        }
+
+        #region UserMethods
+        private void StartCopy()
+        {
+            try
+            {
+                progressBar.Value = 0;
+                CopyFiles();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string GetFilesName()
+        {
+            string namesFrom = string.Empty;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*|Txt files (*.txt)|*.txt";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if(ofd.FileNames.Length > 5)
+                {
+                    MessageBox.Show("Одновременно вы можете копировать не более 5 файлов", "ошибка", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                    return namesFrom;
+                }
+                foreach (var item in ofd.FileNames)
+                {
+                    pathesFrom.Add(item);                    
+                    namesFrom += item;
+                    namesFrom += "; ";
+                }
+            }
+            return namesFrom;
+        }
+
+        private void GetFilesNames(string str)
+        {
+            //pathesFrom = new List<string>();
+            var match = Regex.Matches(str, @"\w\:[\\\w]*\.\w*(?=\;)");
+
+            //for (int i = 0; i < match.Count; i++)
+            //{
+            //    pathesFrom.Add(match[i].Value);
+            //}
+            foreach (var item in match)
+            {
+                pathesFrom.Add(item.ToString());
+            }
+        }
+
+        private string GetFolderName()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                return fbd.SelectedPath;
+            }
+            return string.Empty;
+        }
+
+        private void CopyFile(Object obj)
+        {
+            string fileName = obj.ToString();
+            string destFile = Path.GetFileName(fileName);
+            byte[] buffer = new byte[4096];
+            using (FileStream sourceStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                double fileLength = sourceStream.Length;
+                using (FileStream destStream = new FileStream(String.Format("{0}\\{1}", pathTo, destFile), FileMode.Create, FileAccess.Write))
+                {
+                    int currentBlockSize = 0;
+                    do
+                    {
+                        currentBlockSize = sourceStream.Read(buffer, 0, buffer.Length);
+                        destStream.Write(buffer, 0, currentBlockSize);
+                    } while (currentBlockSize > 0);
+                }
+            }
+        }
+
+        private void CopyFiles()
+        {
+            if (string.IsNullOrEmpty(tbFrom.Text) || string.IsNullOrEmpty(tbTo.Text))
+            {
+                MessageBox.Show("проверьте путь", "ошибка", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return;
+            }
+            foreach (var fileName in pathesFrom)
+            {
+                tasksLst.Add(Task.Factory.StartNew(CopyFile, fileName));
+                this.Invoke(new Action<int>(ShowOnProgressBar), pathesFrom.Count);
+            }        
+        }
+
+        private void ShowOnProgressBar(int value)
+        {
+            if (progressBar.Value == 0)
+                progressBar.Maximum = value;            
+            progressBar.Increment(1);
+        }
+
+        private void CancellationCopy()
+        {
+            tokenSource.Cancel();
+            foreach (var item in tasksLst)
+            {
+                item.Wait(token);
+            }
+            foreach (var item in pathesFrom)
+            {
+                File.Delete(string.Format("{0}\\{1}", pathTo, Path.GetFileName(item)));
+            }
+            progressBar.Value = 0;
+            MessageBox.Show("Копирование прервано", "Предупреждение", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        #endregion
+    }
+}
+
